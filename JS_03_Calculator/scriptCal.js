7b344e619666cc7ded86bb601e52fbd10af7c64b
Shashank SIngh <60248212+shashanksinghgautam@users.noreply.github.com>
2022-04-16 18:42:18 +0530

    Update scriptCal.js

--- JS_03_Calculator/scriptCal.js
+++ JS_03_Calculator/scriptCal.js
@@ -1,15 +1,68 @@
 var displayArea = document.getElementById("display");
-
+let dotflag=false;
 function isOperator(buttonText) {
-  if (
-    buttonText == "+" ||
-    buttonText == "-" ||
-    buttonText == "*" ||
-    buttonText == "/" ||
-    buttonText == "%"
-  )
+  if (buttonText == "+" || buttonText == "-" || buttonText == "*" || buttonText == "/" || buttonText == "%"){
+    dotflag=false;
     return true;
-  else return false;
+  }
+  else 
+    return false;
+}
+ 
+
+
+function btnClicked(buttonText) {
+
+  if(displayArea.innerText.length >= 24)
+    return
+
+  if (buttonText === "CLR") {
+    displayArea.innerText = ""
+    return
+  }
+
+  if (buttonText === "DEL") {
+    displayArea.innerText = displayArea.innerText.slice(0, -1)
+    return
+  }
+
+  if (buttonText === "="){
+    try {
+      displayArea.innerText = eval(displayArea.innerText);
+    } catch (error) {
+      displayArea.innerText = 0;
+    }
+    return
+  }
+  
+  if(buttonText=='.' && displayArea.innerText.includes('.') && dotflag){
+    return
+  }
+  
+
+  if (displayArea.innerText === "0") {
+    if (buttonText == "0" || buttonText == "00")
+      return
+
+    if (buttonText == "." || isOperator(buttonText)) {
+      displayArea.innerText = '0' + buttonText;
+      return
+    }
+
+    displayArea.innerText = buttonText;
+  } 
+  else {
+
+    if(buttonText=='.')
+      dotflag=true;
+    
+    if (isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator(buttonText)) {
+      displayArea.innerText =displayArea.innerText.slice(0, -1) + buttonText
+      return
+    }
+    
+    displayArea.innerText = displayArea.innerText + buttonText;
+  }
 }
 
 /* ---------------------------------------------------------------- */
@@ -55,54 +108,6 @@ function isOperator(buttonText) {
 // }
 
 /* ---------------------------------------------------------------- */
-
-function btnClicked(buttonText) {
-  if(displayArea.innerText.length >= 24)
-    return
-
-  if (buttonText === "CLR") {
-    displayArea.innerText = "0"
-    return
-  }
-
-  if (buttonText === "DEL") {
-    displayArea.innerText = displayArea.innerText.slice(0, -1)
-    return
-  }
-
-  if (buttonText === "="){
-    try {
-      displayArea.innerText = eval(displayArea.innerText);
-    } catch (error) {
-      displayArea.innerText = 0;
-    }
-    return
-  }
-
-  if (displayArea.innerText === "0") {
-    if (buttonText == "0" || buttonText == "00")
-      return
-
-    if (buttonText == "." || isOperator(buttonText)) {
-      displayArea.innerText = '0' + buttonText;
-      return
-    }
-
-    displayArea.innerText = buttonText;
-  } else {
-    if (
-      isOperator(displayArea.innerText[displayArea.innerText.length - 1]) &&
-      isOperator(buttonText)
-    ) {
-      displayArea.innerText =
-        displayArea.innerText.slice(0, -1) + buttonText
-      return
-    }
-
-    displayArea.innerText = displayArea.innerText + buttonText;
-  }
-}
-
 /* ---------------------------------------------------------------- */
 
 // function btnClicked(buttonText) {
